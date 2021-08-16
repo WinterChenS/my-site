81624ad981b3136f1e91c4d80e91eef21c36caf6
winterchen <1085143002@qq.com>
2021-08-16 10:33:08 +0800

    Create docker-build.sh

---
+++ docker-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ X$1 = X ]; then
+        read -p "请输入镜像版本号(按回车默认latest)：" version
+else
+        version=$1
+fi
+
+if [ X$version = X ]; then
+version=latest
+fi
+
+echo -e "\n"
+echo "------------------------"
+echo "镜像版本为：$version"
+echo "------------------------"
+
+
+mvn -DskipTests=true package docker:build &&
+
+
+docker build -f="Dockerfile-push" -t="winterchen/my-site:$version" . &&
+
+docker push winterchen/my-site:$version
+echo "[上传完成]"
